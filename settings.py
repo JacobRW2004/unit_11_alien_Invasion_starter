dfc32c20ae606b2a554f2d6684b20c60fab49124
Jacob Wilczynski <jwilczynski@student.cscc.edu>
2025-04-19 23:18:22 -0400

    p4 - added a new font for the hud

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ class Settings:
         self.text_color = (255,255,255)
         self.button_font_size = 48 
         self.HUD_font_size = 20
-        self.font_file = Path.cwd() / 'Assets' / 'fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
+        self.font_file = Path.cwd() / 'Assets' / 'fonts' / 'Silkscreen' / 'stocky.ttf'
 
     
     def initalize_dynamic_settings(self):
